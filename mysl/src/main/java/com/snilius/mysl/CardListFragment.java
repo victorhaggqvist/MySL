5e49a4c93d00ff070bbbd47ce4f27ea23f0425d6
Victor Häggqvist <victor@snilius.com>
2014-10-25 16:57:27 +0200

    no log import

--- mysl/src/main/java/com/snilius/mysl/CardListFragment.java
+++ mysl/src/main/java/com/snilius/mysl/CardListFragment.java
@@ -7,7 +7,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
