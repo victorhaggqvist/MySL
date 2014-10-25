c90b82bc4c33745c85e457bf11ef7e78aeaff78a
Victor Häggqvist <victor@snilius.com>
2014-10-25 16:31:43 +0200

    needs to be started to use set()

--- mysl/src/main/java/com/snilius/mysl/MainActivity.java
+++ mysl/src/main/java/com/snilius/mysl/MainActivity.java
@@ -53,8 +53,8 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (!BuildConfig.DEBUG)
-            Fabric.with(this, new Crashlytics());
+//        if (!BuildConfig.DEBUG)
+        Fabric.with(this, new Crashlytics());
 
         setContentView(R.layout.activity_main);
 
